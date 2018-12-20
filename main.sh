d10f5accd642089ebeaa113cbea5ab1f270ad53a
Doron Mor <mobclown@gmail.com>
2018-12-20 12:41:25 +0200

    first commit

---
+++ main.sh
@@ -0,0 +1,19 @@
+alias gi='git init'
+alias ga='git add'
+alias gaa='git add .'
+alias gc='git commit -m'
+alias gm='git merge'
+alias gps='git push'
+alias gpsom='git push origin master'
+alias gf='git fetch'
+alias gpl='git pull'
+alias gplom='git pull origin master'
+alias gs='git status'
+alias gco='git checkout'
+alias gcob='git checkout -b'
+alias gb='git branch'
+alias gbd='git branch -d'
+alias nah='git reset --hard;git clean -df;'
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias gln="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit -n"
+alias gls="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit -n 10"
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Git Aliases
+
+Here you can find my Git aliases that I use.  
+Feel free to use it and modify it as you need.  
+If you have any suggestion please let me know =]
+
+## License
+
+This project is licensed under the [MIT license](https://opensource.org/licenses/MIT).
+
+
+Have fun!
